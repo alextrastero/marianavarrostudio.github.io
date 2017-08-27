ff3fe16a9925758f7bb634f8d924b106f69bb1c7
Alexander Odell <alextrastero@gmail.com>
2017-08-27 12:24:08 +0200

    Added font and removes

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,7 @@ import { h } from 'preact';
 const App = () => {
   return (
     <div>
-      APP was here too when? pepepepe
-      <h2>what PSD?</h2>
+      <p>Lorem ipsum dolor sit amet, urna lectus, nibh parturient amet, fusce ac. Pharetra et lacus ullamcorper cras porta purus. Aenean dolor. Aenean at, ante mauris interdum luctus amet. Iaculis praesent donec et lectus tortor, id maecenas montes nam, tortor purus. Vel semper et mauris et tempor, aliquet nec dolor dignissim, est neque erat laoreet sed. Dictumst in nunc. Sed et imperdiet praesent tempor diam. Lacinia sodales vel est, dolor vestibulum est et gravida odio. Mattis nam, tincidunt lorem quis nam mattis, elementum placerat, consectetuer dolor ornare vehicula, etiam dui eu eu mi sit.</p>
     </div>
   );
 };
